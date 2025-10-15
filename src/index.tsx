fa188a3d42bf8366dc749c946c3aec0185e498a6
avaice <dev@cho-ice.xyz>
2025-10-15 23:46:02 +0900

    init sandbox

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+import { App } from './App'
+
+const container = document.getElementById('root')
+if (!container) {
+  throw new Error('Failed to find the root element')
+}
+const root = ReactDOM.createRoot(container)
+root.render(<App />)
