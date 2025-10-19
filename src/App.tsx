3fa53e26411c3c8284af1c4443eb010fefbb7433
avaice <dev@cho-ice.xyz>
2025-10-19 20:44:07 +0900

    feat: implement demo spa

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
 import { lazyImport, useCreateRoutes } from 'packages/neouter/src'
 import './global.css'
+import { Suspense } from 'react'
+import { Header } from './components/Header'
+import { About } from './routes/About'
+import { Home } from './routes/Home'
+import { Post } from './routes/Post'
+import { User } from './routes/User'
 
 const { Lazy } = lazyImport(() => import('./routes/lazy'), 'Lazy')
 
 const routes = {
   '/': {
-    component: () => <div>Home</div>,
+    component: Home,
   },
   '/about': {
-    component: () => <div>About</div>,
+    component: About,
   },
   '/lazy': {
     component: Lazy,
   },
+  '/users/:userId': {
+    component: User,
+  },
+  '/users/:userId/posts/:postId': {
+    component: Post,
+  },
 }
 
 export const App = () => {
-  const { Router } = useCreateRoutes({ routes })
-  return <Router />
+  const { Router, RouterProvider } = useCreateRoutes({ routes })
+  return (
+    <RouterProvider>
+      <div className="mx-auto max-w-[800px] p-4">
+        <Header />
+        <div className="mt-4">
+          <Suspense fallback={<div>Loading...</div>}>
+            <Router />
+          </Suspense>
+        </div>
+      </div>
+    </RouterProvider>
+  )
 }
