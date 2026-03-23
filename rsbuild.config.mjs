9afbc7106afe37c348908833dd74414a75648557
avaice <dev@cho-ice.xyz>
2026-03-23 20:32:51 +0900

    fix: fix title

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -3,6 +3,9 @@ import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
   plugins: [pluginReact()],
+  html: {
+    title: "Hello, neouter!!"
+  },
   module: {
     rules: [
       {
