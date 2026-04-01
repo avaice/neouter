4c47e1814aab6171757bbf888d7b0d0fff971bd0
avaice <dev@cho-ice.xyz>
2026-04-01 09:02:09 +0900

    fix: update Redirect component to use RouterContext for location management

--- packages/neouter/src/libs/redirect.tsx
+++ packages/neouter/src/libs/redirect.tsx
@@ -1,12 +1,13 @@
-import { useLayoutEffect } from 'react'
+import { useContext, useLayoutEffect } from 'react'
 import type { Path } from '../'
-import { useRouter } from '../'
+import { RouterContext } from '../context'
 
 const Redirect = ({ path }: { path: Path }) => {
-  const [, setLocation] = useRouter()
+  const { setLocation: _setLocation } = useContext(RouterContext)
   useLayoutEffect(() => {
-    setLocation(path)
-  }, [path, setLocation])
+    _setLocation(path)
+    window.history.replaceState({}, '', path)
+  }, [path, _setLocation])
   return null
 }
 
